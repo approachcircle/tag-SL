1bd4b815b5dbba48cc9e82f2966b003fd0163fff
approachcircle <osc-mcd@protonmail.com>
2021-02-15 21:01:08 +0000

    add functions and classes

---
+++ main.py
@@ -0,0 +1,30 @@
+# A copy of the apache license is included below to prevent
+# the LICENSE file from needing to be distributed alongside
+# this file as a standalone file.
+'''
+Copyright 2021 approachcircle
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+import os, time
+
+class get:
+    def sessionState(self):
+        pass
+
+class do:
+    def lockSession(self):
+        pass
+
+    def unlockSession(self):
+        pass
